b6b243cc28a22d4f58b57a2990e51c7e11ab9827
Lorm1 <georgeangel04@gmail.com>
2019-12-29 03:47:01 +0200

    .

--- src/main/java/me/george/astoria/game/player/PlayerConnection.java
+++ src/main/java/me/george/astoria/game/player/PlayerConnection.java
@@ -1,15 +1,21 @@
 package me.george.astoria.game.player;
 
+import me.george.astoria.Astoria;
 import me.george.astoria.game.Rank;
 import me.george.astoria.game.nation.Nation;
 import me.george.astoria.networking.database.DatabaseAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.UUID;
 
@@ -30,9 +36,38 @@ public class PlayerConnection implements Listener {
         Player pl = event.getPlayer();
         APlayer player = getInstanceOfPlayer(pl);
 
+        event.setJoinMessage(null);
+
+        if (player.getRank().equals(Rank.ADMIN)) {
+            Astoria._hiddenPlayers.add(player.getPlayer());
+            player.getPlayer().setGameMode(GameMode.CREATIVE);
+            player.getPlayer().setInvulnerable(true);
+
+            for (Player pla : Bukkit.getOnlinePlayers()) {
+                if (getInstanceOfPlayer(pla).isStaff())
+                    pla.sendMessage(player.getDisplayName() + ChatColor.GREEN + " has joined.");
+            }
+        }
+
+        for (Player p : Astoria._hiddenPlayers) {
+            pl.hidePlayer(p);
+            p.setPlayerListName(null);
+        }
+
+        if (!pl.hasPlayedBefore()) {
+            pl.sendMessage("welcome");
+            pl.sendActionBar("welcome"); // debug
+
+            pl.teleport(Nation.HUMANS.getSpawnLocation());
+            pl.playSound(pl.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 5, 5);
+            pl.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20 * 5, 50));
+            pl.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 3, 35));
+        }
+
         // TODO: Database access on-join ?
         player.setRank(Rank.ADMIN); // DEBUG
         player.setNation(Nation.ORCS); // DEBUG
+
     }
 
     @EventHandler
@@ -41,6 +76,7 @@ public class PlayerConnection implements Listener {
         APlayer player = getInstanceOfPlayer(pl);
 
         // TODO: Database save player data.
+        Astoria._hiddenPlayers.remove(pl);
         APlayer.getAstoriaPlayers().remove(pl.getUniqueId(), player);
     }
 }
